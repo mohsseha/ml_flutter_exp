c8369d61306b6ef0ee2d34178e2b1dd07a980b26
Husain Al-Mohssen <halmohssen@irobot.com>
2021-06-11 21:49:08 -0400

    initial commit, random experiments, builds on OSX

---
+++ android/app/src/main/kotlin/com/example/ml_flutter_exp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ml_flutter_exp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,240 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Path Provider',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Path Provider'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<Directory?>? _tempDirectory;
+  Future<Directory?>? _appSupportDirectory;
+  Future<Directory?>? _appLibraryDirectory;
+  Future<Directory?>? _appDocumentsDirectory;
+  Future<Directory?>? _externalDocumentsDirectory;
+  Future<List<Directory>?>? _externalStorageDirectories;
+  Future<List<Directory>?>? _listSlashTmpDirs;
+
+  void _requestTempDirectory() {
+    setState(() {
+      _tempDirectory = getTemporaryDirectory();
+    });
+  }
+
+  Widget _buildDirectory(
+      BuildContext context, AsyncSnapshot<Directory?> snapshot) {
+    SelectableText text = const SelectableText('');
+    if (snapshot.connectionState == ConnectionState.done) {
+      if (snapshot.hasError) {
+        text = SelectableText('Error: ${snapshot.error}');
+      } else if (snapshot.hasData) {
+        text = SelectableText('path: ${snapshot.data!.path}');
+      } else {
+        text = const SelectableText('path unavailable');
+      }
+    }
+    return text; //Padding(padding: const EdgeInsets.all(40.0), child: text);
+  }
+
+  Widget _buildDirectories(
+      BuildContext context, AsyncSnapshot<List<Directory>?> snapshot) {
+    SelectableText text = const SelectableText('');
+    if (snapshot.connectionState == ConnectionState.done) {
+      if (snapshot.hasError) {
+        text = SelectableText('Error: ${snapshot.error}');
+      } else if (snapshot.hasData) {
+        final String combined =
+            snapshot.data!.map((Directory d) => d.path).join(', ');
+        text = SelectableText('paths: $combined');
+      } else {
+        text = const SelectableText('path unavailable');
+      }
+    }
+    return Padding(padding: const EdgeInsets.all(16.0), child: text);
+  }
+
+  void _requestAppDocumentsDirectory() {
+    setState(() {
+      _appDocumentsDirectory = getApplicationDocumentsDirectory();
+    });
+  }
+
+  void _requestAppSupportDirectory() {
+    setState(() {
+      _appSupportDirectory = getApplicationSupportDirectory();
+    });
+  }
+
+  void _requestAppLibraryDirectory() {
+    setState(() {
+      _appLibraryDirectory = getLibraryDirectory();
+    });
+  }
+
+  void _requestExternalStorageDirectory() {
+    setState(() {
+      _externalDocumentsDirectory = getExternalStorageDirectory();
+    });
+  }
+
+  void _requestExternalStorageDirectories(StorageDirectory type) {
+    setState(() {
+      _externalStorageDirectories = getExternalStorageDirectories(type: type);
+    });
+  }
+
+
+
+
+  void _requestExternalCacheDirectories() {
+    setState(() {
+      _listSlashTmpDirs = listSlashTmp();
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: SelectableText(widget.title),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Material(
+                  elevation: 160.0,
+                  shape: BeveledRectangleBorder(
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(46.0)),
+                  ),
+                  child: ElevatedButton(
+                    child: const SelectableText('Get Temporary DirectoryXXXX'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.white,
+                      shadowColor: Colors.red,
+                      elevation: 25,
+                    ),
+                    onPressed: _requestTempDirectory,
+                  )),
+            ),
+            FutureBuilder<Directory?>(
+                future: _tempDirectory, builder: _buildDirectory),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                child:
+                    const SelectableText('Get Application Documents Directory'),
+                onPressed: _requestAppDocumentsDirectory,
+              ),
+            ),
+            FutureBuilder<Directory?>(
+                future: _appDocumentsDirectory, builder: _buildDirectory),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                child:
+                    const SelectableText('Get Application Support Directory'),
+                onPressed: _requestAppSupportDirectory,
+              ),
+            ),
+            FutureBuilder<Directory?>(
+                future: _appSupportDirectory, builder: _buildDirectory),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                child:
+                    const SelectableText('Get Application Library Directory'),
+                onPressed: _requestAppLibraryDirectory,
+              ),
+            ),
+            FutureBuilder<Directory?>(
+                future: _appLibraryDirectory, builder: _buildDirectory),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                child: SelectableText(Platform.isIOS
+                    ? 'External directories are unavailable on iOS'
+                    : 'Get External Storage Directory'),
+                onPressed:
+                    Platform.isIOS ? null : _requestExternalStorageDirectory,
+              ),
+            ),
+            FutureBuilder<Directory?>(
+                future: _externalDocumentsDirectory, builder: _buildDirectory),
+            Column(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  child: SelectableText(Platform.isIOS
+                      ? 'External directories are unavailable on iOS'
+                      : 'Get External Storage Directories'),
+                  onPressed: Platform.isIOS
+                      ? null
+                      : () {
+                          _requestExternalStorageDirectories(
+                            StorageDirectory.music,
+                          );
+                        },
+                ),
+              ),
+            ]),
+            FutureBuilder<List<Directory>?>(
+                future: _externalStorageDirectories,
+                builder: _buildDirectories),
+            Column(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  child: SelectableText(Platform.isIOS
+                      ? 'External directories are unavailable on iOS'
+                      : 'Get External Cache Directories'),
+                  onPressed:
+                      Platform.isIOS ? null : _requestExternalCacheDirectories,
+                ),
+              ),
+            ]),
+            FutureBuilder<List<Directory>?>(
+                future: _listSlashTmpDirs, builder: _buildDirectories),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<List<Directory>?>? listSlashTmp() async {
+    print("attempting to open a folder, have no idea of this works:");
+    return null;
+  }
+}
